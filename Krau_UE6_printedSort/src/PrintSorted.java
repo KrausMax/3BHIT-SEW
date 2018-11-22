1e85c4aeb597f3fc50908b0a0d0d52884b506f37
KrausMax <44222211+Melcuis@users.noreply.github.com>
2018-11-22 10:14:17 +0100

    Commited Übung6 (1) printSorted

--- Krau_UE6_printedSort/src/PrintSorted.java
+++ Krau_UE6_printedSort/src/PrintSorted.java
@@ -3,18 +3,20 @@ import java.util.TreeSet;
 
 public class PrintSorted {
 
-    private static final int SETLENGTH = 10;
+    private static final int SETLENGTH = 10;                            //Konstante für Setlänge
 
     public static void main(String[] args) {
 
-        Set<Integer> gzSet = new TreeSet<>();
+        Set<Integer> gzSet = new TreeSet<>();                           //Set erstellt
 
-        for (int i = 0; i < SETLENGTH; i++) {
+        for (int i = 0; i < SETLENGTH; i++) {                           //Befüllt Set mit zufälligen Nummern
             gzSet.add((int) (Math.random() * 100 + 1));
         }
+        //1. Teil der Ausgabe Original Werte
         System.out.println("Original Werte: " + gzSet);
         System.out.println("----------------------------------------------------------------------------------");
         System.out.print("Mit 10 multiplizierte Werte: ");
+        //2. Teil der Ausgabe gibt die mit 10 multiplizierten Nummern sortiert aus
         for (int element : gzSet) {
             System.out.print(element * 10 + ", ");
         }
